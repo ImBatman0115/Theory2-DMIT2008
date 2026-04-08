f7c0d86a968b80e79f8925d6b3f5a1f0121433bb
ImBatman0115 <kaydenmayhew@gmail.com>
2026-04-08 16:16:10 -0600

    edited next.config

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "storage.ko-fi.com",
-      },
+      }, 
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      }
     ],
   },
 };
