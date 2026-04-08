a559046ef46f1fd1277e8e79023c28f22353e671
ImBatman0115 <kaydenmayhew@gmail.com>
2026-04-08 14:06:10 -0600

    added support link, but its snake

--- src/pages/api/about.js
+++ src/pages/api/about.js
@@ -12,5 +12,5 @@ export default function handler(req, res){
     ],
     passion: "Beyond website development, I like video games, movies, reading, and being active outside, usually bring my beagle, Odin with me. My biggest fixation is NANANANANA BATMAN! I think he is really cool and collect all sorts of merchandise of him.",
     approach: "I try to get things done my way, but if that doesn't work I reach out to other methods. In the end I want a end product I and my client are proud of."
-  }})
+  }});
 }
